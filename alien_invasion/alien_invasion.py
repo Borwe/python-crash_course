296587654e444be2445633ccf5d87d2692228819
Borwe <brian.orwe@gmail.com>
2018-07-08 23:18:41 +0300

    update

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init()
+    screen=pygame.display.set_mode((1200,800))
+    pygame.display.set_caption('Alien Invasion')
+
+    # set the background color.
+    bg_color=(230,230,230)
+
+    # start the main loop for the game
+    while True:
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen during each pass through the loop
+        screen.fill(bg_color)
+
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()
